ba418be4bdb5b431b97c6394b24c040e3f4c3443
Mahendra Netam <mahenranetam02@gmail.com>
2020-07-18 10:55:56 +0530

    by netam 18 jul

---
+++ app/src/main/java/com/netam/loginandregisterscreen/MainActivity.java
@@ -0,0 +1,14 @@
+package com.netam.loginandregisterscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
